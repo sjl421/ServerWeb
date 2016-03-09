0250e00beaad137420bc4ee8fc30f4a1f1ca3fe9
mukhad <mukhad@yandex.ru>
2016-03-09 15:35:16 +0300

    unicorn v0.4

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 def app(environ,start_response):
     status = '200 OK'
-    body = "Hello python"
+    s = environ['QUERY_STRING']
+    body = s.replace("&","\r\n")
     headers = [
         ("Content-Type", "text/plain"),
         ("Contenet-Length",str(len(body)))
